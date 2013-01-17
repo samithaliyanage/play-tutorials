600fce0265bc5a0893c6ff76f50fac186bbdbc13
Liyanage <shliyana@156-56-153-37.dhcp-bl.indiana.edu>
2013-01-17 14:25:23 -0500

    new changes to Application.java

--- todolist/app/controllers/Application.java
+++ todolist/app/controllers/Application.java
@@ -2,13 +2,28 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.data.*;
 
 import views.html.*;
 
 public class Application extends Controller {
+
+  static Form<Task> taskForm = form(Task.class);
   
   public static Result index() {
-    return ok(index.render("Your new application is ready."));
+    return redirect(routes.Application.tasks());
   }
+
+   public static Result tasks(){
+       return ok(views.html.index.render(Task.all(), taskForm));
+   }
+
+    public static Result newTask(){
+        return TODO;
+    }
+
+    public static Result deleteTask(Long id){
+        return TODO;
+    }
   
 }
